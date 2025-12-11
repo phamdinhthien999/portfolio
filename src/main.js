6349b3d814d39dd8dc8e4575f18e468ea330f386
Phạm Đình Thiện <phamdinhthien999@gmail.com>
2025-12-11 21:07:20 +0700

    x

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import * as Sentry from "@sentry/vue";
 
 // Import components
 import AboutSection from './components/AboutSection.vue'
@@ -27,6 +28,14 @@ const router = createRouter({
 
 // Create Vue app
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: "https://92e885b1968027062385aa48ce130505@o4510516558102528.ingest.de.sentry.io/4510516637925456",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 // Use router
 app.use(router)
